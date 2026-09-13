8b30047c80dc0408934fac0245354ab7509f26e2
Saswati M <saswatim867@gmail.com>
2026-09-13 12:43:01 +0530

    Fix invisible text on dashboard pages caused by global dark theme text color

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -40,7 +40,7 @@ export default function DashboardLayout({
   }
 
   return (
-    <div className="flex min-h-screen flex-col bg-gray-100 lg:flex-row">
+    <div className="flex min-h-screen flex-col bg-gray-100 text-gray-900 lg:flex-row">
       <Sidebar />
 
       <main className="flex-1 p-4 sm:p-6 lg:p-8">{children}</main>
